5bdc71ff1228acb964ef9ed4f11fe4f5df8515d7
Nate Chen <natechen.ntu@gmail.com>
2015-01-04 15:47:34 +0800

    Add check for first name, last name and profile name correctness.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :profile_name, presence: true, 
+  						   uniqueness: true,
+  						   format: {
+  						   	 with: /a-zA-Z0-9_-/,
+  						   	 message: 'Must be formatted correctly.'
+  						   }
+
   has_many :statuses
+
   def full_name
   	first_name + " " + last_name
   end
