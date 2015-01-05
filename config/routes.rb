d0aa7a361322aee653e194618b539724530ab96b
Nate Chen <natechen.ntu@gmail.com>
2015-01-05 23:47:33 +0800

    Modify user's profile route

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'feed', to: "statuses#index", as: :feed
   root to: 'statuses#index'
 
-  get "/:id", to: "profiles#show"
+  get "profile/:id", to: "profiles#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
