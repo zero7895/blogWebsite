44d32a4ce686ef314f5e1c70a2fb7f1c3800a545
Nate Chen <natechen.ntu@gmail.com>
2015-04-19 11:02:01 +0800

    Add a button for game info detail

---
+++ app/assets/javascripts/game_data.js
@@ -0,0 +1,8 @@
+
+$(document).ready( function(){
+
+	$('.gameInput').on('change', '.input_money1' , function(){
+		console.log( +$(this).val());
+	});
+
+});
---
+++ app/assets/javascripts/game_infos.js
@@ -0,0 +1,16 @@
+
+$(document).ready( function(){
+
+	$('.gameInfo').on('click', '.gameInfoBtn' , function(){
+		var accPay = $(this).closest('.gameInfo').data('acc');
+		console.log("accPay:" + accPay);
+		var basePay = $(this).closest('.gameInfo').data('base');
+		console.log("basePay:" + basePay);
+
+		var payInfo = '<p>accPay $' + accPay + ', basePay $' + basePay + '</p>';
+   	var gameInfo = $(this).closest('.gameInfo');
+   	gameInfo.append(payInfo);
+   	$(this).remove();
+   	});
+
+});
