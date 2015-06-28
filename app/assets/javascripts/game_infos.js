3459df8ed48fbc88799178a309fa91bdb2d3f6b8
Nate Chen <natechen.ntu@gmail.com>
2015-06-28 20:18:00 +0800

    Add a game info btn to show game's info

--- app/assets/javascripts/game_infos.js
+++ app/assets/javascripts/game_infos.js
@@ -1,16 +1,15 @@
+$(document).ready(function() {
 
-$(document).ready( function(){
+    $('.gameInfo').on('click', '.gameInfoBtn', function() {
+        var accPay = $(this).closest('.gameInfo').data('acc');
+        console.log("accPay:" + accPay);
+        var basePay = $(this).closest('.gameInfo').data('base');
+        console.log("basePay:" + basePay);
 
-	$('.gameInfo').on('click', '.gameInfoBtn' , function(){
-		var accPay = $(this).closest('.gameInfo').data('acc');
-		console.log("accPay:" + accPay);
-		var basePay = $(this).closest('.gameInfo').data('base');
-		console.log("basePay:" + basePay);
-
-		var payInfo = '<p>accPay $' + accPay + ', basePay $' + basePay + '</p>';
-   	var gameInfo = $(this).closest('.gameInfo');
-   	gameInfo.append(payInfo);
-   	$(this).remove();
-   	});
+        var payInfo = '<p>accPay $' + accPay + ', basePay $' + basePay + '</p>';
+        var gameInfo = $(this).closest('.gameInfo');
+        gameInfo.append(payInfo);
+        $(this).remove();
+    });
 
 });
